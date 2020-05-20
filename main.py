fd382134b17dbb575d5e45794cd9ce2e0eda3b7a
zukky <34536327+SuzukiDaishi@users.noreply.github.com>
2020-05-20 18:05:45 +0900

    てんぷら

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home() :
-    html = '<h1>Hello World</h1>'
-    return html
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
