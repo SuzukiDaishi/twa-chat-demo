18ee0247beeb9d39cd96063e2d7480bdaab40ee6
zukky <34536327+SuzukiDaishi@users.noreply.github.com>
2020-05-20 18:46:53 +0900

    けーさん

---
+++ spotsSearch.py
@@ -0,0 +1,23 @@
+def spotSortByDocuments(chatDoc, spotsData) :
+    """
+    # spotSortByDocuments
+
+    下記の用に作ってくれると嬉しい
+
+    - param
+        - chatDoc
+            - type
+                - str
+            - doc
+                - チャットで送られた文章
+        - spotsData: List
+            - type
+                - List
+            - doc
+                - スポットのデータ
+    - ret
+        - sotedSpotsData
+            - ソートしたスポットのデータ
+    """
+
+    pass
