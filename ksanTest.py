63256adb97cc64e432ba29015e89cbea8a738591
fukukei <fukusato86k@gmail.com>
2020-05-22 22:33:35 +0900

    cos類似度出しました

--- ksanTest.py
+++ ksanTest.py
@@ -14,9 +14,28 @@ python ksanTest.py
 from  spotsSearch import *
 import json
 
+
 json_open = open('database/database.json', 'r',encoding='utf-8_sig')
 spotsData = json.load(json_open)
 
-test_word = '桜が見たいでやんす'
-test_out  = spotSortByDocuments(test_word, spotsData)
-print(test_out)
+print(spotsData)
+
+'''
+test_array_A = words_to_sep(spotsData[0]["description"])
+test_out_A = words_to_freqdict(test_array_A)
+print(test_out_A)
+
+test_array_B = words_to_sep(spotsData[1]["description"])
+test_out_B = words_to_freqdict(test_array_B)
+print(test_out_B)
+
+test_word = '桜を見たいな～'
+word_array = words_to_sep(test_word)
+word_out = words_to_freqdict(word_array)
+print(word_out)
+
+out_cosA = calc_cos(test_out_A,word_out)
+out_cosB = calc_cos(test_out_B,word_out)
+print(out_cosA)
+print(out_cosB)
+'''
--- spotsSearch.py
+++ spotsSearch.py
@@ -1,9 +1,14 @@
+from os import path
+import numpy as np
+import MeCab
+import math
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 def spotSortByDocuments(chatDoc, spotsData) :
     """
     # spotSortByDocuments
-
     下記の用に作ってくれると嬉しい
-
     - param
         - chatDoc
             - type
@@ -20,3 +25,44 @@ def spotSortByDocuments(chatDoc, spotsData) :
             - ソートしたスポットのデータ
     """
 	pass
+
+def words_to_sep(text): #分かち書き
+    out_words = []
+    tagger = MeCab.Tagger('-Ochasen')
+    tagger.parse('')
+    node = tagger.parseToNode(text)
+
+    while node:
+        word_type = node.feature.split(",")[0]
+        if word_type in ["名詞"]:
+            out_words.append(node.surface)
+        node = node.next
+    return out_words
+
+def words_to_freqdict(words): #重さつけ的な
+    freqdict = {}
+    for word in words:
+        if word in freqdict:
+            freqdict[word] = freqdict[word] + 1
+        else:
+            freqdict[word] = 1
+    return freqdict
+	
+def calc_cos(dictA, dictB): #cos類似度
+    lengthA = 0.0
+    for key,value in dictA.items():
+        lengthA = lengthA + value*value
+    lengthA = math.sqrt(lengthA)
+
+    lengthB = 0.0
+    for key,value in dictB.items():
+        lengthB = lengthB + value*value
+    lengthB = math.sqrt(lengthB)
+
+    dotProduct = 0.0
+    for keyA,valueA in dictA.items():
+        for keyB,valueB in dictB.items():
+            if keyA==keyB:
+                dotProduct = dotProduct + valueA*valueB
+    cos = dotProduct / (lengthA*lengthB)
+    return cos
